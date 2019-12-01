339ddb3efd821dbe5387f414180a51a60000b6c7
Mehrdad <m.mirsamie@gmail.com>
2019-12-01 14:42:21 +0330

    add course published restriction

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -5,23 +5,23 @@ const Chapter = require('../models/chapter.model')
 
 class CourseController {
   static async index (request, reply) {
-    const courses = await Course.query().select('id', 'name', 'description', 'education_level')/*.where('education_level', request.user.education_level)*/
+    const courses = await Course.query().select('id', 'name', 'description', 'education_level').where('published', 1)/*.where('education_level', request.user.education_level)*/
     reply.send(courses)
   }
   
   static async courseOfEducationLevel (request, reply) {
-    const courses = await Course.query()/*.where('education_level', request.user.education_level)*/.select('id', 'name', 'description', 'education_level')
+    const courses = await Course.query()/*.where('education_level', request.user.education_level)*/.select('id', 'name', 'description', 'education_level').where('published', 1)
     reply.send(courses)
   }
 
   static async chapters (request, reply) {
-    const courses = await Course.query().select(['id', 'name', 'description', 'duel_time']).eager('[ chapters(orderByChapterOrder, defaultSelects) ]').where('education_level', request.user.education_level)
+    const courses = await Course.query().select(['id', 'name', 'description', 'duel_time']).eager('[ chapters(orderByChapterOrder, defaultSelects) ]').where('education_level', request.user.education_level).where('published', 1)
     reply.send(courses)
   }
   
   static async courseChapters (request, reply) {
     // const course = await Course.query().select(['id', 'name', 'description', 'duel_time']).where('id', request.params.courses_id).where('education_level', request.user.education_level).eager('[ chapters(orderByChapterOrder, defaultSelects) ]').first()
-    const course = await Course.query().where('id', request.params.courses_id).first()
+    const course = await Course.query().where('published', 1).where('id', request.params.courses_id).first()
     if(!course) {
       reply.code(404).send({
         statusCode: 404,

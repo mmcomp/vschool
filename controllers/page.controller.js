991211f834ff126fd97dd307f6444a38fa9894f3
Mehrdad <m.mirsamie@gmail.com>
2019-12-09 10:27:21 +0330

    .

--- controllers/page.controller.js
+++ controllers/page.controller.js
@@ -5,7 +5,7 @@ const Page = require('../models/page.model')
 class PageController {
   static async load (request, reply) {
     const page = await Page.query()/*.select(['id', 'question', 'question_type', 'answer', 'choices', 'score', 'solution'])*/.where('id', request.params.id).first()
-    reply.send((page.page)?page.page:null)
+    reply.send((page && page.page)?page.page:null)
   }
 }
 

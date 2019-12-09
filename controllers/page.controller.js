6540c9a526b1afe2b2435c99c5274926f2c093d7
Mehrdad <m.mirsamie@gmail.com>
2019-12-09 10:30:44 +0330

    .

--- controllers/page.controller.js
+++ controllers/page.controller.js
@@ -4,8 +4,16 @@ const Page = require('../models/page.model')
 
 class PageController {
   static async load (request, reply) {
-    const page = await Page.query()/*.select(['id', 'question', 'question_type', 'answer', 'choices', 'score', 'solution'])*/.where('id', request.params.id).first()
-    reply.send((page && page.page)?page.page:null)
+    const page = await Page.query().where('id', request.params.id).first()
+    if(page && page.page) {
+      reply.send(page.page)
+    }else {
+      reply.code(404).send({
+        statusCode: 404,
+        error: "Not Found",
+        message: "صفحه مورد نظر پیدا نشد"
+      })
+    }
   }
 }
 

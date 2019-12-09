cbfcd9b3d49e1cadc218baf51c684c922338ca5b
Mehrdad <m.mirsamie@gmail.com>
2019-12-09 10:44:17 +0330

    .

--- controllers/page.controller.js
+++ controllers/page.controller.js
@@ -6,7 +6,13 @@ class PageController {
   static async load (request, reply) {
     const page = await Page.query().where('id', request.params.id).first()
     if(page && page.page) {
-      reply.send(page.page)
+      let thePage = page.page
+      for(let i = 0;i < thePage.data.length;i++) {
+        if(thePage.data[i].type == 'image') {
+          thePage.data[i].data = `/page_images/page_${page.id}/${thePage.data[i].data}`
+        }
+      }
+      reply.send(thePage)
     }else {
       reply.code(404).send({
         statusCode: 404,

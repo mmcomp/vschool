522c9185fca87192802d8da8791eca69530a39ea
m.mirsamie <m.mirsamie@gmail.com>
2020-02-01 13:29:29 +0330

    finish cron job

--- cronJobs.js
+++ cronJobs.js
@@ -3,10 +3,11 @@ const DuelController = require('./controllers/duel.controller')
 
 console.log('Duel Status job instantiation');
 const job = new CronJob(`* */${process.env.DUEL_STATUS_CHECK} * * * *`,async function() {
-	const d = new Date();
-    console.log('Every second:', d);
+	// const d = new Date();
+    // console.log('Every second:', d);
     try{
-        console.log('Duel Status Result', await DuelController.status())
+        await DuelController.status()
+        // console.log('Duel Status Result', await DuelController.status())
     }catch(e) {
         console.log('Duel Status Error', e)
     }
